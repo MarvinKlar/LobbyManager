4faf22b8ac2a0e0e0dbc5d206acc4a5e4dd59621
Marvin Klar <privat@marvinklar.de>
2018-10-01 22:23:57 +0200

    Added Maven support
    * Added Travis support
    * Added a README
    * Formattet the code

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Build Status](https://travis-ci.org/MarvinKlar/LobbyManager.svg?branch=master)](https://travis-ci.org/MarvinKlar/LobbyManager)
+
+# LobbyManager
+
+A simple Minecraft plugin which allows you to manage your lobbies on a BungeeCord network
--- src/mr/minecraft15/LobbyManager/Main/Main.java
+++ src/mr/minecraft15/LobbyManager/Main/Main.java
@@ -15,61 +15,62 @@ import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
 
 public class Main extends Plugin {
-	public static Plugin plugin;
-	
-	public static File f;
-	public static Configuration c;
-	
-	public static String lang;
-	
-	public static String[] arguments;
-	
-	public void onEnable() {
-		plugin = this;
-		loadConfig();
-		ProxyServer.getInstance().getPluginManager().registerCommand(this, new Lobby());
-	}
-	
-	public static void loadConfig() {
-		try {
-			f = new File(Main.plugin.getDataFolder(), "config.yml");
-			if(!Main.plugin.getDataFolder().exists()) {
-				Main.plugin.getDataFolder().mkdir();
-				if(!f.exists()) {
-					Files.copy(Main.plugin.getResourceAsStream("config.yml"), f.toPath(), new CopyOption[0]);
-				}
-			}
-			c = ConfigurationProvider.getProvider(YamlConfiguration.class).load(f);
+    public static Plugin plugin;
 
-			if(c.get("Aliases") == null) {
-				ArrayList<String> list = new ArrayList<String>();
-				list.add("hub");
-				list.add("leave");
-				list.add("l");
-				c.set("Aliases", list);
-				save();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		lang = c.getString("Language");
-		arguments = new String[c.getStringList("Aliases").size()];
-		int i = 0;
-		for(String alias : c.getStringList("Aliases")) {
-			arguments[i] = alias;
-			i++;
-		}
-	}
+    public static File f;
+    public static Configuration c;
 
-	public static void save() {
-		try {
-			ConfigurationProvider.getProvider(YamlConfiguration.class).save(Main.c, Main.f);
-		} catch (IOException e) {
-			e.printStackTrace();
+    public static String lang;
+
+    public static String[] arguments;
+
+    @Override
+    public void onEnable() {
+	plugin = this;
+	loadConfig();
+	ProxyServer.getInstance().getPluginManager().registerCommand(this, new Lobby());
+    }
+
+    public static void loadConfig() {
+	try {
+	    f = new File(Main.plugin.getDataFolder(), "config.yml");
+	    if (!Main.plugin.getDataFolder().exists()) {
+		Main.plugin.getDataFolder().mkdir();
+		if (!f.exists()) {
+		    Files.copy(Main.plugin.getResourceAsStream("config.yml"), f.toPath(), new CopyOption[0]);
 		}
+	    }
+	    c = ConfigurationProvider.getProvider(YamlConfiguration.class).load(f);
+
+	    if (c.get("Aliases") == null) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("hub");
+		list.add("leave");
+		list.add("l");
+		c.set("Aliases", list);
+		save();
+	    }
+	} catch (Exception e) {
+	    e.printStackTrace();
 	}
-	
-	public static String getMessage(String message) {
-		return ChatColor.translateAlternateColorCodes('&', c.getString("Lang." + lang + "." + message));
+	lang = c.getString("Language");
+	arguments = new String[c.getStringList("Aliases").size()];
+	int i = 0;
+	for (String alias : c.getStringList("Aliases")) {
+	    arguments[i] = alias;
+	    i++;
 	}
+    }
+
+    public static void save() {
+	try {
+	    ConfigurationProvider.getProvider(YamlConfiguration.class).save(Main.c, Main.f);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+    }
+
+    public static String getMessage(String message) {
+	return ChatColor.translateAlternateColorCodes('&', c.getString("Lang." + lang + "." + message));
+    }
 }
